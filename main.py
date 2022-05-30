edbfc01f019cce4a954940165d02908ec4a2daf3
CLOUDUH <clouduh@outlook.com>
2022-05-30 20:56:33 +0800

    turn policies to function

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Author: CLOUDUH
+Date: 2022-05-30 20:55:42
+LastEditors: CLOUDUH
+LastEditTime: 2022-05-30 20:56:03
+Description: 
+'''
+
+from policies import charging_policy
+
+if __name__ == '__main__':
+    charging_policy('data', 'all')
--- policies.py
+++ policies.py
@@ -1,10 +1,3 @@
-'''
-Author: CLOUDUH
-Date: 2020-02-20 00:36:56
-LastEditors: CLOUDUH
-LastEditTime: 2022-05-30 20:47:08
-Description: 
-'''
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -22,50 +15,58 @@ import sys
 import numpy as np
 import surface_points
 
-##############################################################################
+def charging_policy(data_dir, file_name):
+    '''
+    Args: 
+        data_dir: data files dir
+        file_name: csv file name
+    Returns: 
+        None
+    Detail: 
+        Generates all policies within a 4-step policy 
+        space based on your specifications.
+    '''    
 
-# PARAMETERS TO CREATE POLICY SPACE
-C1 = [3.6, 4.0, 4.4, 4.8, 5.2, 5.6, 6.0, 7.0, 8.0]
-C2 = [3.6, 4.0, 4.4, 4.8, 5.2, 5.6, 6.0, 7.0]
-C3 = [3.6, 4.0, 4.4, 4.8, 5.2, 5.6]
+    # PARAMETERS TO CREATE POLICY SPACE
+    C1 = [3.6, 4.0, 4.4, 4.8, 5.2, 5.6, 6.0, 7.0, 8.0]
+    C2 = [3.6, 4.0, 4.4, 4.8, 5.2, 5.6, 6.0, 7.0]
+    C3 = [3.6, 4.0, 4.4, 4.8, 5.2, 5.6]
 
-C4_LIMITS = [0.1, 4.81] # Lower and upper limits specifying valid C4s
+    C4_LIMITS = [0.1, 4.81] # Lower and upper limits specifying valid C4s
 
-DIR = sys.argv[1]
-FILENAME = sys.argv[2]
+    # Pre-initialize arrays and counters
+    policies = -1.*np.ones((1000,4));
+    valid_policies = -1.*np.ones((1000,4));
 
-print(DIR, FILENAME)
+    count = valid_count = 0
 
-##############################################################################
+    # Generate policies
+    for c1, c2, c3 in [(c1,c2,c3) for c1 in C1 for c2 in C2 for c3 in C3]:
+        c4 = 0.2/(1/6 - (0.2/c1 + 0.2/c2 + 0.2/c3))
+        policies[count,:] = [c1, c2, c3, c4]
+        count += 1
+        
+        if c4 >= C4_LIMITS[0] and c4 <= C4_LIMITS[1]:
+            if c1 == 4.8 and c2 == 4.8 and c3 == 4.8:
+                print('baseline') # Exclude baseline
+            else:
+                valid_policies[valid_count,:] = [c1, c2, c3, c4]
+                valid_count += 1
 
-# Pre-initialize arrays and counters
-policies = -1.*np.ones((1000,4));
-valid_policies = -1.*np.ones((1000,4));
+    # Remove trailing zeros
+    policies = policies[0:count]
+    valid_policies = valid_policies[0:valid_count]
 
-count = valid_count = 0
+    # Update user
+    print('Count = ' + str(count))
+    print('Valid count = ' + str(valid_count))
 
-# Generate policies
-for c1, c2, c3 in [(c1,c2,c3) for c1 in C1 for c2 in C2 for c3 in C3]:
-    c4 = 0.2/(1/6 - (0.2/c1 + 0.2/c2 + 0.2/c3))
-    policies[count,:] = [c1, c2, c3, c4]
-    count += 1
-    
-    if c4 >= C4_LIMITS[0] and c4 <= C4_LIMITS[1]:
-        if c1 == 4.8 and c2 == 4.8 and c3 == 4.8:
-            print('baseline') # Exclude baseline
-        else:
-            valid_policies[valid_count,:] = [c1, c2, c3, c4]
-            valid_count += 1
+    ## Save policies
+    np.savetxt(os.path.join(data_dir, 'policies_' + file_name + '.csv'), valid_policies, delimiter=',', fmt='%1.3f')
 
-# Remove trailing zeros
-policies = policies[0:count]
-valid_policies = valid_policies[0:valid_count]
+    surface_points.plot_surface(C1, C2, C3, C4_LIMITS, data_dir, file_name)
 
-# Update user
-print('Count = ' + str(count))
-print('Valid count = ' + str(valid_count))
+    return None
 
-## Save policies
-np.savetxt(os.path.join(DIR, 'policies_' + FILENAME + '.csv'), valid_policies, delimiter=',', fmt='%1.3f')
-
-surface_points.plot_surface(C1, C2, C3, C4_LIMITS, DIR, FILENAME)
+if __name__ == '__main__':
+    charging_policy('data', 'all')
